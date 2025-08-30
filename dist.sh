4aa91de1ee384634f5e42b981561cea76d201f74
jordansafer <jordansafer2@gmail.com>
2025-08-30 18:12:22 -0400

    dont put all images in dist

--- dist.sh
+++ dist.sh
@@ -9,13 +9,23 @@ cd "$ROOT_DIR"
 rm -rf dist volition-dist.zip
 mkdir dist
 
-# Copy everything except dev/builder files
+# Copy everything except dev/builder files and heavy/unused assets
 rsync -a ./ dist/ \
   --exclude ".git" \
   --exclude "dist" \
   --exclude "node_modules" \
   --exclude "*.zip" \
-  --exclude "*.DS_Store"
+  --exclude "*.DS_Store" \
+  # Exclude docs and large media
+  --exclude "docs/" \
+  --exclude "docs/**" \
+  --exclude "*.mp4" \
+  --exclude "*.mov" \
+  --exclude "*.avi" \
+  --exclude "*.gif" \
+  # Exclude unused icon variants/logos (keep icon16/32/48/128 only)
+  --exclude "icons/volition_*" \
+  --exclude "icons/*.svg"
 
 # Create ZIP
 cd dist
